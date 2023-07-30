79acd1c71f4102e34d2b084268a9a3408760400f
nelson <peternelson063@gmail.com>
2023-07-30 13:02:07 +0100

    added an array to data.js

--- src/components/PhoneList.jsx
+++ src/components/PhoneList.jsx
@@ -1,30 +1,4 @@
 // eslint-disable-next-line no-unused-vars
-const phones = [
-  {
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtVyi5Zmm-oOSCm0mATvZu5kciWGIFJgaltQ&usqp=CAU',
-    product: '10c',
-    brand: 'Redmi',
-    id: 1,
-  },
-  {
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgYsfP5OYpJaQ0-WxNw-dwPTyyMDjqLDQE_A&usqp=CAU',
-    product: 'X plus',
-    brand: 'iphone',
-    id: 2,
-  },
-  {
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_kxxITG6kiGTH7a4O00za6UCvSHdEg5d_BA&usqp=CAU',
-    product: '2210',
-    brand: 'Nokia',
-    id: 3,
-  },
-  {
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-eEqhPjhgJXamTePRQGr6Z_vv2_gSQsMoaw&usqp=CAU',
-    product: 's12',
-    brand: 'Samsung',
-    id: 4,
-  },
-];
 
 const PhoneList = () => {
   return <section className='phoneList'></section>;
--- src/data.js
+++ src/data.js
@@ -79,3 +79,30 @@ export const tours = [
     info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
   },
 ];
+
+export const phones = [
+  {
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtVyi5Zmm-oOSCm0mATvZu5kciWGIFJgaltQ&usqp=CAU',
+    product: '10c',
+    brand: 'Redmi',
+    id: 1,
+  },
+  {
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgYsfP5OYpJaQ0-WxNw-dwPTyyMDjqLDQE_A&usqp=CAU',
+    product: 'X plus',
+    brand: 'iphone',
+    id: 2,
+  },
+  {
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_kxxITG6kiGTH7a4O00za6UCvSHdEg5d_BA&usqp=CAU',
+    product: '2210',
+    brand: 'Nokia',
+    id: 3,
+  },
+  {
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-eEqhPjhgJXamTePRQGr6Z_vv2_gSQsMoaw&usqp=CAU',
+    product: 's12',
+    brand: 'Samsung',
+    id: 4,
+  },
+];
